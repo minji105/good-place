bc0fee383e93459a0e03a2042a4f89e5b145d81f
minji_105 <ajhjmj9@naver.com>
2025-07-22 16:14:44 +0900

    ✨ Feat: pass endpoint prop to Section component (#7)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,8 @@ import Section from '@/components/Section';
 export default function Home() {
   return (
     <div>
-      <Section title={'찜한 맛집'} />
-      <Section title={'맛집 목록'} />
+      <Section title={'찜한 맛집'} endpoint={'users/places'} />
+      <Section title={'맛집 목록'} endpoint={'places'} />
     </div>
   );
 }
