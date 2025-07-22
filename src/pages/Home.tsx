608993a74e10b37995825a8077266124995a852f
minji_105 <ajhjmj9@naver.com>
2025-07-22 14:46:23 +0900

    ✨ Feat: add Home Component (#1)

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import Section from '../components/Section';
+
+export default function Home() {
+  return (
+    <div>
+      <Section title={'찜한 맛집'} />
+      <Section title={'맛집 목록'} />
+    </div>
+  );
+}
