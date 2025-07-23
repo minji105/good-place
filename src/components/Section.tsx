1e04008b1fe0bd0ada1bf7476524b8c50f23e5af
minji_105 <ajhjmj9@naver.com>
2025-07-23 14:20:21 +0900

    ✨ Feat: improve loading and empty states (#13)

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -28,23 +28,27 @@ export default function Section({ title, endpoint }: SectionProps) {
     fetchList();
   }, [endpoint]);
 
-  if (loading) return <p>로딩 중...</p>;
-
   return (
-    !loading && (
-      <section className="p-8">
-        <h2 className="mb-4 text-4xl">{title}</h2>
+    <section className="p-8">
+      <h2 className="mb-4 text-4xl">{title}</h2>
 
-        {list.length ? (
-          <div className="grid grid-cols-3 justify-center gap-8 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
-            {list.map((item, index) => (
-              <PlaceCard key={index} item={item} />
-            ))}
-          </div>
-        ) : (
-          <div>목록이 비었습니다.</div>
-        )}
-      </section>
-    )
+      {loading ? (
+        <div className="bg-gray-100 p-8">
+          <p>맛집을 불러오는 중입니다...</p>
+        </div>
+      ) : (
+        <>
+          {list.length ? (
+            <div className="grid grid-cols-3 justify-center gap-8 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
+              {list.map((item, index) => (
+                <PlaceCard key={index} item={item} />
+              ))}
+            </div>
+          ) : (
+            <div className="bg-gray-100 p-8">목록이 비었습니다.</div>
+          )}
+        </>
+      )}
+    </section>
   );
 }
